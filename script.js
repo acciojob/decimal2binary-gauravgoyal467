50c3f7e6f090d1df6c12bf8b9587449765f1426c
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-04-15 18:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function threeSum(arr, target) {
+function decimalToBinary(input) {
 // write your code here
-  
-}
-
-module.exports = threeSum;
+	let ans=[];
+	while(input!=0){
+		if(input%2==1){
+			ans.push(1);
+			input=input-1;	
+		}else{
+			ans.push(0);
+		}
+		input=input/2;
+	}
+	let output;
+	for (let index = ans.length-1; index >=0; index--) {
+		let num=ans[index]*1e(index);
+		output=output+num;
+	}
+	
+  return output;
+ }
+let input=prompt("Enter a number");
+alert(decimalToBinary(input));
+module.exports = decimalToBinary;
