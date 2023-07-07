34fe227eb9d81d0a956970357650fd3d4df3941a
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 15:15:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function decimalToBinary(input) {
-// write your code here
-	let ans=[];
-	while(input!=0){
-		if(input%2==1){
-			ans.push(1);
-			input=input-1;	
-		}else{
-			ans.push(0);
-		}
-		input=input/2;
-	}
-	let output=0;
-	for (let index = ans.length-1; index >=0; index--) {
-		let num=ans[index]*Math.pow(10,index);
-		output=output+num;
-	}
-  return output;
- }
-module.exports = decimalToBinary;
+function decimalToBinary(num) {
+  // Write your code here
+}
+
+window.decimalToBinary = decimalToBinary;
