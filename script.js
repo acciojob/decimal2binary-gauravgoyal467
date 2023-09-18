5ff517b0017a77fff3ba64601ed60fdf81ca21da
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-09-18 11:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function decimalToBinary(num) {
-  // Write your code here
+  let bin = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 2;
+        x = parseInt(x / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
 }
 
 window.decimalToBinary = decimalToBinary;
